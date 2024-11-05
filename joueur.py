a9683e8293ea0dd8af131467258cae09656b6118
Soufiane <s2elmams@enib.fr>
2024-11-05 15:50:53 +0100

    init

--- joueur.py
+++ joueur.py
@@ -1,6 +1,6 @@
-class JOUEUR:
-    def __init__(self,max_speed: list[int] = [0, 0], vitesse: list[int] = [0, 0], animation_frame: int = 0, jump_authorized:bool = True):
-        self.max_speed = max_speed
+class Joueur:
+    def __init__(self,MAX_SPEED: list[int] = [0, 0], vitesse: list[int] = [0, 0], animation_frame: int = 0, jump_authorized:bool = True):
+        self.MAX_SPEED = MAX_SPEED
         self.vitesse = vitesse
         self.animation_frame = animation_frame
         self.jump_authorized = jump_authorized
---
+++ main.py
@@ -0,0 +1,9 @@
+import pygame
+
+pygame.init()
+
+fenetre = pygame.display.set_mode((640,480))
+while True:
+    pass
+
+
