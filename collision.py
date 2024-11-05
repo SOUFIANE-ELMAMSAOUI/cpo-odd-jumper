22536c92f5eaa5c65dd32dcd129e1e73d3778d75
Soufiane <s2elmams@enib.fr>
2024-11-05 16:15:31 +0100

    init

--- collision.py
+++ collision.py
@@ -5,3 +5,4 @@ class Collision:
 
     def test_collision(self):
         pass
+
---
+++ entity.py
@@ -0,0 +1,13 @@
+class Entity:
+    def __init__(self,MAX_SPEED: list[int] = [0, 0], vitesse: list[int] = [0, 0]):
+        self.MAX_SPEED = MAX_SPEED
+        self.vitesse = vitesse
+
+    def animation(self):
+        pass
+
+    def move(self):
+        pass
+
+    def animation(self):
+        pass
