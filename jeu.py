bbe3cc4b7b383bc92a63e1019822a205825cb5f6
b1lelann <b1lelann@enib.fr>
2024-11-05 16:28:34 +0100

    ajout class image et suite jeu et main

--- jeu.py
+++ jeu.py
@@ -1,13 +1,22 @@
-from joueur import Joueur
+import pygame
+ #from joueur import Joueur
 
 class Jeu:
     def __init__(self):
         self.etat = 0 # 0 = menu principale
-        self.joueur = Joueur([10, 10], [0, 0])
+        #self.joueur = Joueur([10, 10], [0, 0])
+        pygame.init()
 
 
     def run(self):
-        pass
+        # boucle de simulation
+        fenetre = pygame.display.set_mode((640, 480))
+        loop = True
+        while loop:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    loop = False
+
 
     def load_levels(self):
         pass
