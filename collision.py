149cf0daf9e5ba6f061f100f1fb27fd89ed0f57f
Soufiane <s2elmams@enib.fr>
2024-11-05 16:05:24 +0100

    init

---
+++ collision.py
@@ -0,0 +1,7 @@
+class Collision:
+    def __init__(self,position:[int] = [0,0],size:[int] = [0,0]):
+        self.position = position
+        self.size = size
+
+    def test_collision(self):
+        pass
