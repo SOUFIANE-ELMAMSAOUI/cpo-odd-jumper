01b76ee577c495c73fbfd0a1d97341d173f4c14c
b1lelann <b1lelann@enib.fr>
2024-11-06 12:32:18 +0100

    ajout d'un class bouton pour crée les menu plus facilement

---
+++ bouton.py
@@ -0,0 +1,66 @@
+import pygame
+
+
+class Bouton:
+    def __init__(self, size=[150, 50], text_bouton="Bouton", font= None, position = [10, 10], fenetre = None):
+        self.size = size
+        self.text_bouton = text_bouton
+        self.button_surface = pygame.Surface(size)
+        self.font = font
+        self.text = self.font.render(self.text_bouton, True, (0, 0, 0))
+        self.text_rect = self.text.get_rect(center=(self.button_surface.get_width() / 2, self.button_surface.get_height() / 2))
+        self.button_rect = pygame.Rect(position[0], position[1], position[0]+size[0], position[1]+size[1])
+        self.fenetre = fenetre
+
+    def animation(self):
+        if self.button_rect.collidepoint(pygame.mouse.get_pos()):
+            pygame.draw.rect(self.button_surface, (127, 255, 212), (1, 1, self.size[0]-2, self.size[1]-2))
+        else:
+            pygame.draw.rect(self.button_surface, (255, 255, 255), (1, 1, self.size[0]-2, self.size[1]-2))
+
+        self.button_surface.blit(self.text, self.text_rect)
+        self.fenetre.blit(self.button_surface, (self.button_rect.x, self.button_rect.y))
+
+
+
+
+
+
+#test
+if __name__ == "__main__":
+    pygame.init()
+    clock = pygame.time.Clock()
+
+    window_size = (500, 400)
+    fenetre = pygame.display.set_mode(window_size)
+    pygame.display.set_caption('Test Bouton')
+    clock.tick(60)
+
+    font = pygame.font.Font(None, 24)
+
+    bouton = Bouton([150, 40], "Quitter", font, fenetre= fenetre)
+
+    loop = True
+    while loop:
+
+
+        # Fill the display with color
+        fenetre.fill((155, 255, 155))
+
+        # Get events from the event queue
+        for event in pygame.event.get():
+            # Check for the quit event
+            if event.type == pygame.QUIT:
+                # Quit the game
+                loop = False
+
+            # Check for the mouse button down event
+            if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+                # Call the on_mouse_button_down() function
+                if bouton.button_rect.collidepoint(event.pos):
+                    loop = False
+
+        # Check if the mouse is over the button. This will create the button hover effect
+        bouton.animation()
+        # Update the game state
+        pygame.display.update()
--- fct_test/button.py
+++ fct_test/button.py
@@ -4,7 +4,7 @@ import sys
 # Initialize Pygame
 pygame.init()
 
-clock=pygame.time.Clock()
+clock = pygame.time.Clock()
 
 # Create a Pygame window
 window_size = (1920, 1280)
@@ -19,47 +19,45 @@ button_surface = pygame.Surface((150, 50))
 
 # Render text on the button
 text = font.render("Quitter", True, (0, 0, 0))
-text_rect = text.get_rect(center=(button_surface.get_width()/2, button_surface.get_height()/2))
-
+text_rect = text.get_rect(center=(button_surface.get_width() / 2, button_surface.get_height() / 2))
 
 # Create a pygame.Rect object that represents the button's boundaries
 button_rect = pygame.Rect(125, 125, 150, 50)  # Adjust the position as needed
 
 # Start the main loop
 while True:
- # Set the frame rate
- clock.tick(60)
- 
- # Fill the display with color
- screen.fill((155, 255, 155))
+    # Set the frame rate
+    clock.tick(60)
 
- # Get events from the event queue
- for event in pygame.event.get():
-  # Check for the quit event
-  if event.type == pygame.QUIT:
-   # Quit the game
-   pygame.quit()
-   sys.exit()
+    # Fill the display with color
+    screen.fill((155, 255, 155))
 
-  # Check for the mouse button down event
-  if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
-   # Call the on_mouse_button_down() function
-   if button_rect.collidepoint(event.pos):
-    pygame.quit()
-    sys.exit()
+    # Get events from the event queue
+    for event in pygame.event.get():
+        # Check for the quit event
+        if event.type == pygame.QUIT:
+            # Quit the game
+            pygame.quit()
+            sys.exit()
 
- # Check if the mouse is over the button. This will create the button hover effect
- if button_rect.collidepoint(pygame.mouse.get_pos()):
-  pygame.draw.rect(button_surface, (127, 255, 212), (1, 1, 148, 48))
- else:
-  pygame.draw.rect(button_surface, (255, 255, 255), (1, 1, 148, 48))
+        # Check for the mouse button down event
+        if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+            # Call the on_mouse_button_down() function
+            if button_rect.collidepoint(event.pos):
+                pygame.quit()
+                sys.exit()
 
- 
- # Shwo the button text
- button_surface.blit(text, text_rect)
+    # Check if the mouse is over the button. This will create the button hover effect
+    if button_rect.collidepoint(pygame.mouse.get_pos()):
+        pygame.draw.rect(button_surface, (127, 255, 212), (1, 1, 148, 48))
+    else:
+        pygame.draw.rect(button_surface, (255, 255, 255), (1, 1, 148, 48))
 
- # Draw the button on the screen
- screen.blit(button_surface, (button_rect.x, button_rect.y))
+    # Shwo the button text
+    button_surface.blit(text, text_rect)
 
- # Update the game state
- pygame.display.update()
+    # Draw the button on the screen
+    screen.blit(button_surface, (button_rect.x, button_rect.y))
+
+    # Update the game state
+    pygame.display.update()
