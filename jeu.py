3815e565e6ff060fee3c358509d5a7ed36387c8a
b1lelann <b1lelann@enib.fr>
2024-11-20 11:40:36 +0100

    ajout des différent menu et des command des boutons

--- jeu.py
+++ jeu.py
@@ -1,5 +1,6 @@
 import pygame
- #from joueur import Joueur
+from menu import Menu
+#from joueur import Joueur
 
 class Jeu:
     def __init__(self):
@@ -16,22 +17,42 @@ class Jeu:
         self.fenetre = pygame.display.set_mode(self.window_size, pygame.FULLSCREEN)
         pygame.display.set_caption('ODD jumper')
 
+        #création des menus TEMPORAIRE
+        self.menu_1 = Menu("./menu/menu_principal.json",0,  self.fenetre)
+        self.menu_1.create_boutons()
+
+        self.menu_niveau = Menu("./menu/menu_niveau.json", 1, self.fenetre)
+        self.menu_niveau.create_boutons()
+
+        self.menu_badge = Menu("./menu/menu_badge.json", 2, self.fenetre)
+        self.menu_badge.create_boutons()
+
         self.loop = True
 
 
     def run(self):
         # boucle de simulation
         while self.loop:
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    self.loop = False
 
-            if self.etat == 0:
-                #menu
-                self.main_menu()
+            if self.etat == -1:
+                #bouton quitté
+                self.loop = False
 
-    def main_menu(self):
-        pass
+            elif self.etat == 0:
+                #menu principale
+                self.loop, self.etat = self.menu_1.run()
+
+            elif self.etat == 1:
+                #menu des niveaux
+                self.loop, self.etat = self.menu_niveau.run()
+
+            elif self.etat == 2:
+                #menu des badges
+                self.loop, self.etat = self.menu_badge.run()
+
+
+            #on update l'écran avec la nouvelle fram
+            pygame.display.update()
 
     def load_levels_data(self):
         pass
--- menu.py
+++ menu.py
@@ -3,35 +3,37 @@ import pygame
 from bouton import Bouton
 
 class Menu:
-    def __init__(self, path, fenetre):
+    def __init__(self, path, etat,  fenetre):
         self.path = path
         self.boutons = []
         self.fenetre = fenetre
         self.font = pygame.font.Font(None, 24)
+        self.etat = etat
 
 
     def create_boutons(self):
         file = open(self.path, 'r')
         data = json.load(file)
         for bouton_data in data["boutons"]:
-            self.boutons.append(Bouton(bouton_data["size"], bouton_data["text"], self.font, bouton_data["position"], self.fenetre))
+            self.boutons.append(Bouton(bouton_data["size"], bouton_data["text"], self.font, bouton_data["position"], bouton_data["command"], self.fenetre))
 
 
     def run(self):
-        self.fenetre.fill((155, 255, 155))
+        self.fenetre.fill((150, 150, 150))
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 # Quit the game
-                return False
+                return (False, -1)
             # Check for the mouse button down event
             if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
                 # Call the on_mouse_button_down() function
-                for b in menu_test.boutons:
+                for b in self.boutons:
                     if b.button_rect.collidepoint(event.pos):
-                        print(b.text_bouton)
-        for b in menu_test.boutons:
+                        return (True, b.command)
+        for b in self.boutons:
             b.animation()
-        return True
+        return (True, self.etat)
+
 
 
 if __name__ == "__main__":
@@ -52,6 +54,6 @@ if __name__ == "__main__":
     loop = True
 
     while loop:
-        loop = menu_test.run()
+        loop, e = menu_test.run()
         # Update the game state
         pygame.display.update()
