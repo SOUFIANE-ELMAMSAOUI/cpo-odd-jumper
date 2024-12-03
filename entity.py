bc3c75e801054bea9dd1eda31e31bbe1cb84bb06
Soufiane <s2elmams@enib.fr>
2024-12-03 15:27:34 +0100

    test affichage entités

--- entity.py
+++ entity.py
@@ -2,18 +2,16 @@ from collision import Collision
 from spritesheet import Spritesheet
 
 class Entity:
-    def __init__(self,MAX_SPEED = [0, 0], vitesse= [0, 0], name = "", path_image = "", position= [0, 0], size = [0, 0]):
+    def __init__(self,MAX_SPEED = [0, 0], vitesse= [0, 0], name = "", path_image = "", position= [0, 0], size = [0, 0],fenetre = None):
         self.MAX_SPEED = MAX_SPEED
         self.vitesse = vitesse
         self.spritesheet = Spritesheet(path_image)
         self.name = name
         self.collision = Collision(position, size)
+        self.fenetre = fenetre
 
     def animation(self):
-        pass
+        self.spritesheet.image.draw(self.fenetre, self.collision.position)
 
     def move(self):
-        pass
-
-    def animation(self):
         pass
